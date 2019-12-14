d6db5f259bd0d4b13cc0fc51d0de19a7bc6568ad
Peeyush Karnwal <peeyush.karnwa@appinventiv.com>
2019-12-14 18:26:29 +0530

    Initial commit

---
+++ JBInspect/JBInspect.h
@@ -0,0 +1,19 @@
+//
+//  JBInspect.h
+//  JBInspect
+//
+//  Created by Admin on 14/12/19.
+//  Copyright © 2019 Infinite Dreamz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JBInspect.
+FOUNDATION_EXPORT double JBInspectVersionNumber;
+
+//! Project version string for JBInspect.
+FOUNDATION_EXPORT const unsigned char JBInspectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JBInspect/PublicHeader.h>
+
+
